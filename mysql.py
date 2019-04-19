a9e9758123dbf8b13600320dffdef805fb60a2cb
ProfessorV <hugoaguiar70@hotmail.com>
2019-04-19 11:21:24 -0200

    Versão 0.2

--- mysql.py
+++ mysql.py
@@ -2,24 +2,24 @@
 import requests
 
 def sitAtual():
-    r = requests.get('http://ffc3ed57.ngrok.io/atoms')
+    r = requests.get('http://e9c0adf2.ngrok.io/atoms')
     print r.json()
 
 def insert(id, name, numatom, description):
     descricao = open(description)
     descricao = descricao.read()
-    r = requests.post('http://ffc3ed57.ngrok.io/atom', data={'id': id,'element': name, 'numatom': numatom, 'description': descricao})
+    r = requests.post('http://e9c0adf2.ngrok.io/atom', data={'id': id,'element': name, 'numatom': numatom, 'description': descricao})
     print r.status_code
     print r.text
 def delete(id):
-    link = 'http://ffc3ed57.ngrok.io/atom/' + id
+    link = 'http://e9c0adf2.ngrok.io/atom/' + id
     r = requests.delete(link)
     print r.status_code
     print r.text
 def atualizar(id, name, numatom, description):
     descricao = open(description)
     descricao = descricao.read()[:-1]
-    r = requests.patch('http://ffc3ed57.ngrok.io/atom/'+str(id), data={'id': id ,'element': name, 'numatom': numatom, 'description': descricao})
+    r = requests.patch('http://e9c0adf2.ngrok.io/atom/'+str(id), data={'id': id ,'element': name, 'numatom': numatom, 'description': descricao})
     print r.status_code
     print r.text
 def main():
